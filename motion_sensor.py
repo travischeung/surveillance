a5bd5b3453b7de6cffe12e83d897bcde1938caf1
travischeung <46611278+travischeung@users.noreply.github.com>
2022-05-28 17:03:54 -0400

    update email

--- motion_sensor.py
+++ motion_sensor.py
@@ -13,28 +13,17 @@ bot = commands.Bot(command_prefix="!")
 serverID = 920395859617800252
 print('bot')
 
-@bot.event  # idk what this means but you have to do it
-async def on_ready():  # when the bot is ready
-    print("on ready running")
-    guild = discord.utils.get(bot.guilds, name=936704754275483738)
-    print('{bot.user} has joined this cringe discord.')
-    channel2 = bot.get_channel(936704754275483742)
-    await channel2.send("quiet bot")
-
-@bot.event  # idk what this means but you have to do it
-async def detected(ctx):
-    print('message')
-    channel2 = bot.get_channel(936704754275483742)
-    await ctx.send("alert")
-
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
 
 @bot.event  # idk what this means but you have to do it
-async def run(ctx):
+async def on_ready():  # when the bot is ready
+    print("on ready running")
+    guild = discord.utils.get(bot.guilds, name=936704754275483738)
+    print('{bot.user} has joined this cringe discord.')
+    channel2 = bot.get_channel(936704754275483742)
     print("now running")
     while True:
         i=GPIO.input(8)
@@ -44,8 +33,9 @@ async def run(ctx):
             time.sleep(0.1)
         elif i==1:               #When output from motion sensor is HIGH
             print ("Intruder detected"),i
-            await detected(ctx)
-            GPIO.output(3, 1)  #Turn ON LED
+            print('message')
+            channel2 = bot.get_channel(936704754275483742)
+            await channel2.send("alert")
             time.sleep(0.1)
 
 bot.run(TOKEN)
