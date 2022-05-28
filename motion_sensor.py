bda5af6a743d76b4c473a3b149831bc986d34f89
travischeung <46611278+travischeung@users.noreply.github.com>
2022-05-28 16:44:33 -0400

    s

--- motion_sensor.py
+++ motion_sensor.py
@@ -26,16 +26,18 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
-while True:
-    i=GPIO.input(8)
-    if i==0:                 #When output from motion sensor is LOW
-        print ("No intruders"),i
-        GPIO.output(3, 0)  #Turn OFF LED
-        time.sleep(0.1)
-    elif i==1:               #When output from motion sensor is HIGH
-        print ("Intruder detected"),i
-        detected()
-        GPIO.output(3, 1)  #Turn ON LED
-        time.sleep(0.1)
+async def run():
+    while True:
+        i=GPIO.input(8)
+        if i==0:                 #When output from motion sensor is LOW
+            print ("No intruders"),i
+            GPIO.output(3, 0)  #Turn OFF LED
+            time.sleep(0.1)
+        elif i==1:               #When output from motion sensor is HIGH
+            print ("Intruder detected"),i
+            await detected()
+            GPIO.output(3, 1)  #Turn ON LED
+            time.sleep(0.1)
 
 
+run()
